a077bd7a9d3396d1ccf4285e1f84ddf7eecc6d17
Skorodinskaya <skorodinskaya01@gmail.com>
2021-08-24 23:43:22 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import FormAnotherVariant from "./components/forms/FormAnotherVariant";
 export default function App() {
   return (
     <div>
-    {/*<Form/>*/}
-<FormAnotherVariant/>
+    <Form/>
+{/*<FormAnotherVariant/>*/}
     </div>
   );
 }
--- src/components/forms/Form.js
+++ src/components/forms/Form.js
@@ -1,23 +1,22 @@
-import Cars from "../cars/Cars";
 import {useState} from "react";
 import {postCar} from "../services/api.service";
+import Cars from "../cars/Cars";
 
 export default function Form() {
-    const [formState, setFormState] = useState({model: '', price: '', year: ''})
-
+    const [space, setSpace] = useState({model: '', price: '', year: ''})
     let onChangeForm = (e) => {
-        setFormState({...formState, [e.target.name]: e.target.value})
+        setSpace({...space, [e.target.name]: e.target.value})
     };
     let save = (e) => {
-        e.preventDefault()
-        postCar(formState)
+        e.preventDefault();
+        postCar(space)
     };
     return (
         <div>
             <form onSubmit={save}>
-                <input type={'text'} name={'model'} value={formState.model} onChange={onChangeForm}/>
-                <input type={'number'} name={'price'} value={formState.price} onChange={onChangeForm}/>
-                <input type={'number'} name={'year'} value={formState.year} onChange={onChangeForm}/>
+                <input type={'text'} name='model' value={space.model} onChange={onChangeForm} maxLength={20}/>
+                <input type={'number'} name ='price' value={space.price} onChange={onChangeForm} min={1}/>
+                <input type={'number'} name ='year' value={space.year} onChange={onChangeForm} min ={1990} max={2021}/>
                 <input type={'submit'}/>
             </form>
             <Cars/>
