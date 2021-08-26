3290e5eb5f264e41e5b71272106ffde0ca64bc46
anast <skorodinskaya01@gmail.com>
2021-08-26 15:03:43 +0300

    Some changes practice 7

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import {useReducer} from "react";
+// стоврити об'єкт {a:0,b:0,c:0}
+// Стоврити 6 кнопок.
+//     по дві на кожен з параметрів об'єкту.
+// одна кнопка збільшує значення , інша зменшує
+// логіку реалізувати через reducer
+
 import reducer from "./reducers/reducer";
 
 export default function App() {
-    const [{a}, dispatch] = useReducer(reducer, {a:0})
-  return (
-    <div>
-        <h1>Object 1 = {a}</h1>
-        <button onClick={() => dispatch({object: '1', act: '+'})}>Increase</button>
-        <button onClick={() => dispatch({object: '1', act: '-'})}>Decrease</button>
-
-    </div>
-  );
+    const [{a, b, c, dispatch}] = useReducer(reducer, {a: 0, b: 0, c: 0})
+    return (
+        <div>
+            <h2>Object 1 - {a}</h2>
+           <button onClick={() => dispatch ({object: 1, act: '+' }) }>Increase</button>
+           <button onClick={() => dispatch ({object: 1, act: '-' }) }>Increase</button>
+            <h2>Object 1 - {b}</h2>
+           <button onClick={() => dispatch ({object: 1, act: '+' }) }>Increase</button>
+           <button onClick={() => dispatch ({object: 1, act: '-' }) }>Increase</button>
+            <h2>Object 1 - {c}</h2>
+           <button onClick={() => dispatch ({object: 1, act: '+' }) }>Increase</button>
+           <button onClick={() => dispatch ({object: 1, act: '-' }) }>Increase</button>
+        </div>
+    );
 }
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,10 +1,21 @@
 const reducer = (state, action) => {
-    if (action.object === '1' && action.act === '+') {
-        return {...state, a: state.a++}
+    if(action.act === '+') {
+        return{...state, a: state.a + action.object}
+    } else if(action.act === '-') {
+        return {...state, a: state.a - action.object}
     }
-    if (action.object === '1' && action.act ==='-') {
-        return {...state, a: state.a--}
+
+    if (action.act === '+') {
+        return {...state, b: state.b + action.object}
+    } else if (action.act === '-') {
+        return {...state, b: state - action.object}
+    }
+
+    if (action.act === '+') {
+        return {...state, c: state.c + action.object}
+    } else if (action.act === '-') {
+        return {...state, c: state.c - action.object}
     }
     return {...state}
-}
+ }
 export default reducer
